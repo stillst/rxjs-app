b8ed7c0ae848d6e5eff652c3bf54b6e245dd0b5f
s.gavrilov <stanislav.gavrilov@binomo.com>
2019-04-16 19:10:00 +0300

    change ui-code

--- src/app/pages/creation/interval/interval.component.ts
+++ src/app/pages/creation/interval/interval.component.ts
@@ -16,11 +16,11 @@ export class IntervalComponent implements OnInit {
 
   createStreams(): void {
     this.streams = [
-      getStreamObj(interval(1000), 'interval(1000)', 'Стреляет раз в секунду'),
-      getStreamObj(interval(2000), 'interval(2000)', 'Стреляет раз в 2 секунды'),
-      getStreamObj(interval(3000), 'interval(3000)', 'Стреляет раз в 3 секунды'),
-      getStreamObj(interval(4000), 'interval(4000)', 'Стреляет раз в 4 секунды'),
-      getStreamObj(interval(5000), 'interval(5000)', 'Стреляет раз в 5 секунды'),
+      getStreamObj(interval(1000), `interval(1000)`, `Стреляет раз в секунду`),
+      getStreamObj(interval(2000), `interval(2000)`, `Стреляет раз в 2 секунды`),
+      getStreamObj(interval(3000), `interval(3000)`, `Стреляет раз в 3 секунды`),
+      getStreamObj(interval(4000), `interval(4000)`, `Стреляет раз в 4 секунды`),
+      getStreamObj(interval(5000), `interval(5000)`, `Стреляет раз в 5 секунды`),
     ];
   }
 }
--- src/app/ui/ui-code/ui-code.component.ts
+++ src/app/ui/ui-code/ui-code.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ui-code',
-  templateUrl: './ui-code.component.html',
+  templateUrl: './ui-code.component.pug',
   styleUrls: ['./ui-code.component.scss']
 })
 export class UiCodeComponent {
